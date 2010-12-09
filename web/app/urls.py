b09a2e21fccf62244256a03de9180b54b332dd26
lalalainexd <lalalainexd@1783f179-91b5-6867-892f-c6e3ac0b0089>
2010-12-09 02:32:05 +0000

    blah having issues with the web stuff. deleting it again
    git-svn-id: http://pdfainspector.googlecode.com/svn/trunk@178 1783f179-91b5-6867-892f-c6e3ac0b0089

--- web/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('app.views',
-        (r'^upload_file','genReport'),
-        (r'^success','success'),
-        (r'^report/(?P<reportID>\d+)','renderReport'),
-        (r'$','index'),
-
-)
