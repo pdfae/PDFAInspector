88042b8b1b286923463b0391fe565c6b20694d4c
lalalainexd@gmail.com <lalalainexd@gmail.com@1783f179-91b5-6867-892f-c6e3ac0b0089>
2011-03-30 13:29:28 +0000

    left this stuff out by accident
    git-svn-id: http://pdfainspector.googlecode.com/svn/trunk@282 1783f179-91b5-6867-892f-c6e3ac0b0089

---
+++ webv2/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls.defaults import *
+from django.contrib.auth import views as auth_views
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+
+    url(r'^accounts/', include('web.registration.backends.default.urls')),
+    url(r'^pdf/', include('web.pdfevaluator.urls')),
+	url(r'^$',
+		auth_views.login, {'template_name': 'index.html'},
+		name='auth_login'),
+)
