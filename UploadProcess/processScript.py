62d28612ce3ae74317e11c697a7f63bb160fd796
Prateek Arora <prateek1.55@gmail.com>
2012-03-12 02:15:52 -0500

    fixing branches

--- UploadProcess/processScript.py
+++ UploadProcess/processScript.py
@@ -13,13 +13,12 @@ if (len(sys.argv) > 4):
 	pdf_file = "\"" + filepath + filename + "\""
 	parse_file = "\"" + filepath + "json-" + filename.rpartition('.pdf')[0] + ".json" + "\""
 	result_file = filepath + "result-" + filename.rpartition('.pdf')[0] + ".json"
-	result_file_object = open(result_file,'w')
 	
+	result_file_object = open(result_file,'w')
 	command1 = "java -jar " + PDF_JAR + " " + pdf_file
 	command2 = "python2.7 "+ PYTHON_SCRIPT + " " + parse_file
 	
 	p = subprocess.Popen(shlex.split(command1))
-	
 	p.wait()
 	q = subprocess.Popen(shlex.split(command2), stdout = result_file_object)
 	
@@ -27,3 +26,4 @@ if (len(sys.argv) > 4):
 	
 	q.wait()
 	result_file_object.close()
+	
