67042d6ca008567d44819ae69d23eb1aab35e91e
atulgupte <atul.guptey@gmail.com>
2012-03-13 09:39:25 -0500

    Update www-pdfa/pdfa/reports/views.py

--- www-pdfa/pdfa/reports/views.py
+++ www-pdfa/pdfa/reports/views.py
@@ -152,22 +152,14 @@ def displaysummary(request, uid):
 					
 		output = []
 		output.append("<b>Document Level Rules:</b><br><table class = \"fancy\"><tr><th>Title</th><th>Pass</th><th>Fail</th><th>Warning</th><th>Manual Inspection</th></tr>")
-		output.append("<b>Links:</b><br><table class = \"fancy\"><tr><th>Title</th><th>Pass</th><th>Fail</th><th>Warning</th><th>Manual Inspection</th></tr>")
-		output.append("<b>Images:</b><br><table class = \"fancy\"><tr><th>Title</th><th>Pass</th><th>Fail</th><th>Warning</th><th>Manual Inspection</th></tr>")
-		output.append("<b>Forms:</b><br><table class = \"fancy\"><tr><th>Title</th><th>Pass</th><th>Fail</th><th>Warning</th><th>Manual Inspection</th></tr>")
+		output.append("<b>Links:</b><br><table class = \"fancy\"><tr><th>Title</th><th>Pass</th><th>Fail</th><th>Warning</th><th>Manual Inspection</th></tr><tr><td><center>Number of Links</center></td><td><center>" + unicode(numLinks) + "</center></td><td></td><td></td><td></td></tr>")
+		output.append("<b>Images:</b><br><table class = \"fancy\"><tr><th>Title</th><th>Pass</th><th>Fail</th><th>Warning</th><th>Manual Inspection</th></tr><tr><td><center>Number of Image Tags</center></td><td><center>" + unicode(numImages) + "</center></td><td></td><td></td><td></td></tr>")
+		output.append("<b>Forms:</b><br><table class = \"fancy\"><tr><th>Title</th><th>Pass</th><th>Fail</th><th>Warning</th><th>Manual Inspection</th></tr><tr><td><center>Number of Form Elements</center></td><td><center>" + unicode(numForms) + "</center></td><td></td><td></td><td></td></tr>")
 		output.append("<b>Headers:</b><br><table class = \"fancy\"><tr><th>Title</th><th>Pass</th><th>Fail</th><th>Warning</th><th>Manual Inspection</th></tr>")
-		output.append("<b>Tables:</b><br><table class = \"fancy\"><tr><th>Title</th><th>Pass</th><th>Fail</th><th>Warning</th><th>Manual Inspection</th></tr>")
+		output.append("<b>Tables:</b><br><table class = \"fancy\"><tr><th>Title</th><th>Pass</th><th>Fail</th><th>Warning</th><th>Manual Inspection</th></tr><tr><td><center>Number of Table Elements</center></td><td><center>" + unicode(numTables) + "</center></td><td></td><td></td><td></td></tr>")
 		for test in tests:
 			for i in range (0, 5):
 				if (test["category"]==i):
-					if (i==1):
-						output[i] += "<tr><td><center>" + "Number of Links" + "</center></td>" + "<td><center>" + unicode(numLinks) + "</center></td>"+ "<td></td>"+ "<td></td>"+ "<td></td></tr>"
-					elif (i==2):
-						output[i] += "<tr><td><center>" + "Number of Image Tags" + "</center></td>" + "<td><center>" + unicode(numImages) + "</center></td>"+ "<td></td>"+ "<td></td>"+ "<td></td></tr>"
-					elif (i==3):
-						output[i] += "<tr><td><center>" + "Number of Form Elements" + "</center></td>" + "<td><center>" + unicode(numForms) + "</center></td>"+ "<td></td>"+ "<td></td>"+ "<td></td></tr>"
-					elif (i==5):
-						output[i] += "<tr><td><center>" + "Number of Table Elements" + "</center></td>" + "<td><center>" + unicode(numTables) + "</center></td>"+ "<td></td>"+ "<td></td>"+ "<td></td></tr>"
 					if (len(test["tags"])) >= 1:
 						numPass=0
 						numFail=0
