3ccc4d20db309a213325690df83039c039b8a02e
atulgupte <atul.guptey@gmail.com>
2012-03-13 10:44:20 -0500

    Update www-pdfa/pdfa/upload/forms.py

--- www-pdfa/pdfa/upload/forms.py
+++ www-pdfa/pdfa/upload/forms.py
@@ -4,3 +4,6 @@ class uploadfileform (forms.Form):
 	title = forms.CharField(max_length = 130, label = "Report Title", required = False)
 	file = forms.FileField()
 	notes = forms.CharField(max_length = 2000, widget=forms.Textarea, required = False)
+
+class notesupdateform (forms.Form):
+	notes = forms.CharField(max_length = 2000, widget=forms.Textarea, required = False)
