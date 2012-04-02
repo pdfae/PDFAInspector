c85a6313e14813e98ebdb617aa1ceaa7e04b12ce
Prateek Arora <prateek1.55@gmail.com>
2012-04-02 03:03:08 -0500

    views

--- www-pdfa/pdfa/reports/views.py
+++ www-pdfa/pdfa/reports/views.py
@@ -42,6 +42,94 @@ def setup_notes_form(request, uid, notes, fileObj):
 	return form
 	
 # tab to display tree view
+
+def displaysummary(request, uid):
+	currentTab = "summary"
+	[auth, currentPage, parsefile, resultfile, title, notes, fileObj] = setup(request.user, uid)
+	
+	if (os.path.isfile(parsefile) and os.path.isfile(resultfile)):	
+		
+		resultFP = open(resultfile)
+		result_data = json.load(resultFP)
+		resultFP.close()
+		
+		parseFP = open(parsefile)
+		parse_data = json.load(parseFP)
+		parseFP.close()
+		
+		message = "<span color=\"red\">The document is untagged and not accessible</span>"
+		
+		rnum = {}
+		rtitle = {}
+		rtest={}
+		rpass={}
+		rfail={}
+		rinspect={}
+		for i in range(0,6):
+			rnum[i] = 0
+			rtitle[i] = []
+			rtest[i] = []
+			rpass[i] = []
+			rfail[i] = []
+			rinspect[i] = []
+		
+		tests = result_data["results"]	
+		for test in tests:
+			tags = test["tags"]
+			category = test["category"]
+			ntest = 0
+			npass = 0
+			nfail = 0
+			nins = 0
+			for tag in tags:		
+				ntest += 1
+				if (tag["result"]==1):
+					npass += 1
+					if test["id"] == "core.DocumentMustBeTagged":
+						message = "<span color = \"#FFA500\">The document is partially accessible</span>"
+				elif (tag["result"]==2):
+					nfail += 1
+				elif (tag["result"]==3):
+					nins += 1	
+			rnum[category] += 1
+			rtitle[category].append([test["title"], npass, nfail, nins])
+			rtest[category].append(ntest)
+			rpass[category].append(npass)
+			rfail[category].append(nfail)
+			rinspect[category].append(nins)
+				
+		content = []
+		content.append(["Document Level Rules:"])
+		content.append(["Links:"])
+		content.append(["Figures:"])
+		content.append(["Forms:"])
+		content.append(["Headers:"])
+		content.append(["Tables:"])
+		
+		tot_test = 0
+		tot_pass = 0
+		tot_fail = 0
+		tot_ins = 0
+		for i in range(0,6):
+			if sum(rtest[i]) > 0:
+				content[i].append(sum(rtest[i])/rnum[i])
+				content[i].append(rtitle[i])
+			else:
+				content[i].append("")
+			
+			tot_test += sum(rtest[i])
+			tot_pass += sum(rpass[i])
+			tot_fail += sum(rfail[i])
+			tot_ins += sum(rinspect[i])
+		
+		if tot_fail == 0:
+			message = "<span color = \"green\">The document is accessible</span>"	
+		form = setup_notes_form(request, uid, notes, fileObj)
+		return render_to_response("reports/summaryview.html", locals(), context_instance=RequestContext(request))
+	else:
+		return render_to_response("reports/summary_notfound.html", locals())
+
+
 def displaytreeview(request, uid):
 	currentTab = "tree"
 	[auth, currentPage, parsefile, resultfile, title, notes, fileObj] = setup(request.user, uid)
@@ -55,6 +143,12 @@ def displaytreeview(request, uid):
 	return render_to_response("reports/treeview.html", locals())
 
 # tab to display information about tables in document
+
+def displaylinks(request, uid):
+	currentTab = "links"
+	[auth, currentPage, parsefile, resultfile, title, notes, fileObj] = setup(request.user, uid)
+	tableRows = getLink(parsefile, resultfile, uid)
+	return render_to_response("reports/rowView.html", locals())
 
 def displaytables(request, uid):
 	currentTab = "tbl"
@@ -113,91 +207,7 @@ def displayheaders(request, uid):
 	return render_to_response("reports/headerview.html", locals())
 
 # tab to display information about headers in document
-def displaysummary(request, uid):
-	currentTab = "summary"
-	[auth, currentPage, parsefile, resultfile, title, notes, fileObj] = setup(request.user, uid)
-	
-	if (os.path.isfile(parsefile) and os.path.isfile(resultfile)):	
-		
-		resultFP = open(resultfile)
-		result_data = json.load(resultFP)
-		resultFP.close()
-		
-		parseFP = open(parsefile)
-		parse_data = json.load(parseFP)
-		parseFP.close()
-		
-		message = "<font color=\"red\">The document is untagged and not accessible</font>"
-		
-		rnum = {}
-		rtitle = {}
-		rtest={}
-		rpass={}
-		rfail={}
-		rinspect={}
-		for i in range(0,6):
-			rnum[i] = 0
-			rtitle[i] = []
-			rtest[i] = []
-			rpass[i] = []
-			rfail[i] = []
-			rinspect[i] = []
-		
-		tests = result_data["results"]	
-		for test in tests:
-			tags = test["tags"]
-			category = test["category"]
-			ntest = 0
-			npass = 0
-			nfail = 0
-			nins = 0
-			for tag in tags:		
-				ntest += 1
-				if (tag["result"]==1):
-					npass += 1
-					if test["id"] == "core.DocumentMustBeTagged":
-						message = "<font color = \"#FFA500\">The document is partially accessible</font>"
-				elif (tag["result"]==2):
-					nfail += 1
-				elif (tag["result"]==3):
-					nins += 1	
-			rnum[category] += 1
-			rtitle[category].append([test["title"], npass, nfail, nins])
-			rtest[category].append(ntest)
-			rpass[category].append(npass)
-			rfail[category].append(nfail)
-			rinspect[category].append(nins)
-				
-		content = []
-		content.append(["Document Level Rules:"])
-		content.append(["Links:"])
-		content.append(["Figures:"])
-		content.append(["Forms:"])
-		content.append(["Headers:"])
-		content.append(["Tables:"])
-		
-		tot_test = 0
-		tot_pass = 0
-		tot_fail = 0
-		tot_ins = 0
-		for i in range(0,6):
-			if sum(rtest[i]) > 0:
-				content[i].append(sum(rtest[i])/rnum[i])
-				content[i].append(rtitle[i])
-			else:
-				content[i].append("")
-			
-			tot_test += sum(rtest[i])
-			tot_pass += sum(rpass[i])
-			tot_fail += sum(rfail[i])
-			tot_ins += sum(rinspect[i])
-		
-		if tot_fail == 0:
-			message = "<font color = \"green\">The document is accessible</font>"	
-		form = setup_notes_form(request, uid, notes, fileObj)
-		return render_to_response("reports/summaryview.html", locals(), context_instance=RequestContext(request))
-	else:
-		return render_to_response("reports/summary_notfound.html", locals())
+
 
 # tab to display information about bookmarks in document
 
@@ -217,11 +227,6 @@ def displaybookmark(request, uid):
 	output += "</div>"
 	return render_to_response("reports/treeview.html", locals())
 
-def displaylinks(request, uid):
-	currentTab = "links"
-	[auth, currentPage, parsefile, resultfile, title, notes, fileObj] = setup(request.user, uid)
-	output = getLinkOutput(parsefile, resultfile, uid)
-	return render_to_response("reports/formview.html", locals())
 
 def displayformtree(request, uid):
 	currentTab = "formtree"
