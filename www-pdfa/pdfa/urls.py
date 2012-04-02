881a36b31309a6ce18b77c63f787f0caa47bb696
atulgupte <atul.guptey@gmail.com>
2012-04-01 23:15:31 -0500

    added four lines, to enable password reset. lets see if this works.

--- www-pdfa/pdfa/urls.py
+++ www-pdfa/pdfa/urls.py
@@ -16,6 +16,13 @@ urlpatterns = patterns('',
 	
     #Registration
     (r'^accounts/', include('registration.urls')),
+    
+    (r'^accounts/password/reset/$', 'django.contrib.auth.views.password_reset', 
+        {'post_reset_redirect' : '/accounts/password/reset/done/'}),
+(r'^accounts/password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+(r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', 
+        {'post_reset_redirect' : '/accounts/password/done/'}),
+(r'^accounts/password/done/$', 'django.contrib.auth.views.password_reset_complete'),
 
     #File Upload
     (r'^upload/', include('upload.urls')),
