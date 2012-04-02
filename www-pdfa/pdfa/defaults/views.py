4780e4b16efa346319f190a9175d2b015a4ec11d
atulgupte <atul.guptey@gmail.com>
2012-04-02 03:52:11 -0500

    Update www-pdfa/pdfa/defaults/views.py

--- www-pdfa/pdfa/defaults/views.py
+++ www-pdfa/pdfa/defaults/views.py
@@ -2,6 +2,7 @@
 # including required modules
 from django.shortcuts import render_to_response
 from settings import *
+from upload.forms import ContactForm
 	
 # about page
 def about(request):
@@ -11,7 +12,30 @@ def about(request):
 	return render_to_response("defaults/about.html", locals())
 
 # contact page
-def contact(request):
-	currentPage = "contact"
-	auth = request.user.is_authenticated()
-	return render_to_response("defaults/contact.html", locals())
+#def contact(request):
+#	currentPage = "contact"
+#	auth = request.user.is_authenticated()
+#	return render_to_response("defaults/contact.html", locals())
+	
+def contact (request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            if form.is_valid():
+    		subject = form.cleaned_data['subject']
+    		message = form.cleaned_data['message']
+    		sender = form.cleaned_data['sender']
+    		cc_myself = form.cleaned_data['cc_myself']
+    		recipients = ['atul.guptey@gmail.com']
+    		if cc_myself:
+        		recipients.append(sender)
+    	from django.core.mail import send_mail
+    	send_mail(subject, message, sender, recipients)
+    	return HttpResponseRedirect('/thanks/') # Redirect after POST
+    
+    else:
+        form = ContactForm() # An unbound form
+
+    return render_to_response('defaults/contact.html', {
+        'form': form,
+    })
