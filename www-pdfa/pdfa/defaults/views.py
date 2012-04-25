38e80c3df97d4fdf0eeca3a9cd3ceaf313040c37
Prateek Arora <prateek1.55@gmail.com>
2012-04-25 16:18:47 -0500

    stylesheet fixes

--- www-pdfa/pdfa/defaults/views.py
+++ www-pdfa/pdfa/defaults/views.py
@@ -28,7 +28,7 @@ def contact (request):
     		message = form.cleaned_data['message']
     		sender = form.cleaned_data['sender']
     		cc_myself = form.cleaned_data['cc_myself']
-    		recipients = ['atul.guptey@gmail.com']
+    		recipients = ['jongund@illinois.edu']
     		if cc_myself:
         		recipients.append(sender)
     	from django.core.mail import send_mail
