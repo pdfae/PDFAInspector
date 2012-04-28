050a3bc35a19212044437e29b43bb824df05f7c1
atulgupte <atul.guptey@gmail.com>
2012-04-28 16:23:13 -0500

    Added comments.

--- www-pdfa/pdfa/upload/models.py
+++ www-pdfa/pdfa/upload/models.py
@@ -1,17 +1,26 @@
-'''
-Created on Mar 2, 2012
+# Author: Prateek Arora
 
-@author: arora6
-'''
 from django.db import models
 from django.contrib.auth.models import User
+
+# Function: content_file_name
+#
+# This funciton is called when a user account is activated.
+# Returns the complete filepath where an uploaded file is to be stored.
+# 
 
 def content_file_name(instance, filename):
     if (instance.owner == ""):
         return '/'.join(["public", filename])
     return '/'.join([instance.owner, filename])
 
-# Create your models here.
+# UserFile model
+#
+# Model that stores the unique ID (uid), file owner (user name), file,
+# report title (title), notes and creation date in the database associated
+# with this tool.
+#
+
 class UserFile(models.Model):
     uid = models.CharField(max_length = 40, editable = False, unique = True)    
     owner = models.CharField(max_length = 30, editable = False)
